63e7a5714b89733ada08fd8959316a96dc9d6c6e
Isoum <x.jordan@live.com>
2014-05-28 06:57:00 -0400

    Update autoMiner.js

--- autoMiner.js
+++ autoMiner.js
@@ -1,35 +1,34 @@
 autoMiner = {};
 autominer.init = function () {
- /*=====================================================================================
+    /*=====================================================================================
                                      Various Functions
 =======================================================================================*/
-  autoMiner.checkTunnels = function() {
-    if (varHasTunnels != 1) {
-      if (varTntAmount == 500) {
-        blowTunnels();
-      }
-      else if (varGunPowerAmount >= 100 && varCopperWireAmount >= 100) {
-        craftTNT();
-      } else if (varCopperWireAmount < 100 && varCopperAmount >= 2500 && varBronzeBarAmount >= 10) {
-        craftBronzeWire();
-      }
-    }
-  }
- /*=====================================================================================
+    autoMiner.checkTunnels = function () {
+        if (varHasTunnels != 1) {
+            if (varTntAmount == 500) {
+                blowTunnels();
+            } else if (varGunPowerAmount >= 100 && varCopperWireAmount >= 100) {
+                craftTNT();
+            } else if (varCopperWireAmount < 100 && varCopperAmount >= 2500 && varBronzeBarAmount >= 10) {
+                craftBronzeWire();
+            }
+        }
+    };
+    /*=====================================================================================
                                  SetInterval Functions
 =======================================================================================*/
-  setInterval(function() {
-    sellAllOres();
-  }, 30000);
-  
-  setInterval(function() {
-    autoMiner.checkTunnels();
-  }, 10000);
-  
-  setInterval(function() {
-    applyMining();
-  }, 10);
-  
+    setInterval(function () {
+        sellAllOres();
+    }, 30000);
+
+    setInterval(function () {
+        autoMiner.checkTunnels();
+    }, 10000);
+
+    setInterval(function () {
+        applyMining();
+    }, 10);
+
 }; //Ends Game.init();
 /*=====================================================================================
                                         Load AutoMiner
