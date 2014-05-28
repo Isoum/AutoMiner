465c49703566a209166501584687750b574d697a
Isoum <x.jordan@live.com>
2014-05-28 08:50:47 -0400

    Update autoMiner.js

--- autoMiner.js
+++ autoMiner.js
@@ -4,18 +4,18 @@
 function setOverrides() {
     var varTntAmount = document.getElementById("tntAmount").innerHTML;
     document.getElementById("clicktomine").innerHTML = varTntAmount;
-    var AutoMiner.varGunPowerAmount = document.getElementById("gunPowerAmount").innerHTML;
-    var AutoMiner.varCopperWireAmount = document.getElementById("copperWireAmount").innerHTML;
-    var AutoMiner.varCopperAmount = document.getElementById("copperAmount").innerHTML;
-    var AutoMiner.varBronzeBarAmount = document.getElementById("bronzeBarAmount").innerHTML;
+    var varGunPowerAmount = document.getElementById("gunPowerAmount").innerHTML;
+    var varCopperWireAmount = document.getElementById("copperWireAmount").innerHTML;
+    var varCopperAmount = document.getElementById("copperAmount").innerHTML;
+    var varBronzeBarAmount = document.getElementById("bronzeBarAmount").innerHTML;
 }
 
 function checkTunnels() {
-        if (AutoMiner.varTntAmount == 500) {
+        if (varTntAmount == 500) {
             blowTunnels();
-        } else if (AutoMiner.varGunPowerAmount >= 100 && AutoMiner.varCopperWireAmount >= 100) {
+        } else if (varGunPowerAmount >= 100 && varCopperWireAmount >= 100) {
             craftTNT();
-        } else if (AutoMiner.varCopperWireAmount < 100 && AutoMiner.varCopperAmount >= 2500 && AutoMiner.varBronzeBarAmount >= 10) {
+        } else if (varCopperWireAmount < 100 && varCopperAmount >= 2500 && varBronzeBarAmount >= 10) {
             craftBronzeWire();
         }
  }
