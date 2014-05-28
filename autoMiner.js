76086b9e80b3f33250d626444e3502a00455c26e
Isoum <x.jordan@live.com>
2014-05-28 06:50:57 -0400

    Update autoMiner.js

--- autoMiner.js
+++ autoMiner.js
@@ -1,7 +1,34 @@
-setInterval(function() {
-  sellAllOres();
-}, 30000);
-
-setInterval(function() {
-  applyMining();
-}, 10);
+autoMiner = {};
+autominer.init = function () {
+ /*=====================================================================================
+                                     Various Functions
+=======================================================================================*/
+  autoMiner.checkTunnels = function() {
+    if (varHasTunnels != 1) {
+      if (varTntAmount == 500) {
+        blowTunnels();
+      }
+      else if (varGunPowerAmount >= 100 && varCopperWireAmount >= 100) {
+        craftTNT();
+      } else if (varCopperWireAmount < 100 && varCopperAmount >= 2500 && varBronzeBarAmount >= 10) {
+        craftBronzeWire();
+      }
+    }
+  }
+  
+ /*=====================================================================================
+                                 SetInterval Functions
+=======================================================================================*/
+  setInterval(function() {
+    sellAllOres();
+  }, 30000);
+  
+  setInterval(function() {
+    autoMiner.checkTunnels();
+  }, 10000);
+  
+  setInterval(function() {
+    applyMining();
+  }, 10);
+  
+}; //Ends Game.init();
